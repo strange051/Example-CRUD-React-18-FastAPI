2314ed1ff51be3cefd11528b071d7502dc5c7f87
strange051 <agrawal.shashwat@proton.me>
2025-06-14 12:32:51 +0530

    changes

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -1,12 +1,23 @@
-import React, { useState, useEffect } from 'react'
-import { Router, Link } from 'react-router-dom'
-import history from './history'
-import Route  from './router'
+// import React, { useState, useEffect } from 'react'
+// import { Router, Link } from 'react-router-dom'
+// import history from './history'
+// import Route  from './router'
+
+// export default function App() {
+//   return (
+//     <Router history={history}>
+//       <Route />
+//     </Router>
+//   )
+// }
+
+import { HashRouter } from 'react-router-dom'
+import Route from './router'
 
 export default function App() {
   return (
-    <Router history={history}>
+    <HashRouter>
       <Route />
-    </Router>
+    </HashRouter>
   )
 }
